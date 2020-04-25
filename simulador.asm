f45054ac4fc3e9e6acfbb10b9b33e7481c95d07a
Edws <eduardos.rocha17@gmail.com>
2020-04-24 22:44:31 -0300

    Decodificação das instruções de manipulação de dados

--- simulador.asm
+++ simulador.asm
@@ -1,7 +1,13 @@
-; O codigo não pode manipular a pilha senao interfere na pílha do programa que esta sendo simulado
+; O código não pode manipular a pilha, senão interfere na pilha do programa que está sendo simulado
 jmp main
+store 0, r0
+load r0, 0
+storei r0, r0
+loadi r0, r0
+loadn r0, #0
+mov sp, r0
 
-; Usei nop porque por algum motivo do alem var #0 nao estava sendo criado
+; Usei nop porque por algum motivo do além var #0 não estava sendo criado
 Stack: nop
 FlagR: nop
 PrgC: nop
@@ -16,12 +22,12 @@ Reg6: nop
 Reg7: nop
 
 main:
-	; Inicializa PrgC com o incio do codigo
-	loadn r7, #main ; Salva o inicio do codigo no r7
+	; Inicializa PrgC com o íncio do código
+	loadn r7, #main ; Salva o ínicio do código no r7
 	store PrgC, r7
 	load r6, PrgC
 
-	; Inicializa Stack com o valor que esta em sp
+	; Inicializa Stack com o valor que está em sp
 	mov r0, sp
 	store Stack, r0
 
@@ -44,4 +50,52 @@ loop:
 	; r0 tem a instrução
 	; r1 tem o opcode
 
+	; Switch das Instruções
+	loadn r2, #49
+	cmp r1, r2
+	jeq _store
+
+	loadn r2, #48
+	cmp r1, r2
+	jeq _load
+
+	loadn r2, #61
+	cmp r1, r2
+	jeq _storei
+
+	loadn r2, #60
+	cmp r1, r2
+	jeq _loadi
+
+	loadn r2, #56
+	cmp r1, r2
+	jeq _loadn
+
+	; Está diferente do PDF
+	loadn r2, #51
+	cmp r1, r2
+	jeq _mov
+fim:
+
 	jmp loop
+
+; Instruções de manipualação de dado
+_store:
+	jmp fim
+
+_load:
+	jmp fim
+
+_storei:
+	jmp fim
+
+_loadi:
+	jmp fim
+
+_loadn:
+	jmp fim
+
+_mov:
+	jmp fim
+
+code:
