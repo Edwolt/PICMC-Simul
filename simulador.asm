041329e3450645da6799c27ca2347a84a6f795d3
Edws <eduardos.rocha17@gmail.com>
2020-04-24 23:36:27 -0300

    Reorganização do Código
    Agora o Program Counter guarda o valor virtual e não o real

--- simulador.asm
+++ simulador.asm
@@ -1,12 +1,23 @@
-; O código não pode manipular a pilha, senão interfere na pilha do programa que está sendo simulado
-jmp main
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;; O código não pode manipular a pilha, senão interfere na pilha do programa que está sendo simulado ;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-Stack: var #1
+; O programa simulado tem uma memória de tamanho menor, pois o código do simulador ocupa uma parte da memória do simulador
 
-FlagR: var #1
+; Inicializa Stack com o valor que está em sp
+mov r0, sp ; Pega o valor da stack, pois nela iniciará a stack do programa a ser simulado
+sub r0, #code ; Transforma o valor real do Stack Pointer para o valor virtual
+store Stack, r0
 
-PrgC: static PrgC + #0, #code
+jmp loop
 
+Stack: var #1 ; Guarda o Stack Pointer do programa simulado
+			  ; 
+			  ; A Stack terá que começar com um valor menor do que costuma
+			  ; pois na memória não tem espaço para todo o programa
+			  ; por causa que parte está sendo gasta com o código do simulador
+FlagR: var #1 ; Guarda o Flag Register do programa simulado
+PrgC: var #1  ; Guarda o Program Counter do programa simulado
 Regs: var #8
 	static Regs + #0, #0
 	static Regs + #1, #0
@@ -17,23 +28,17 @@ Regs: var #8
 	static Regs + #6, #0
 	static Regs + #7, #0
 
-main:
-	; Inicializa Stack com o valor que está em sp
-	mov r0, sp
-	store Stack, r0
 
-	breakp
 loop:
-	; Pega o valor que PrgC aponta
-	load r1, PrgC
-	loadi r0, r1
+	; Pega o valor que Program Counter aponta
+	load r1, PrgC ; Valor real do Program Counter
+	loadn r2, #code
+	add r2, r1, r2 ; Transforma o valor virtual do Program counter para o valor real
+	loadi r0, r1 ; Carrega valor apontado pelo Program Counter (valor real)
 
-	load r6, PrgC
-
-
-	; Incrementa PrgC
-	inc r1
-	store PrgC, r1
+	; Incrementa valor virtual do Program Counter e o salva na memória
+	inc r2
+	store PrgC, r2
 
 	; r0 tem a instrução
 
@@ -45,7 +50,7 @@ loop:
 	; r1 tem o opcode
 
 	breakp
-	; Switch das Instruções
+	; Switch das Instruções (depois do jeq o r1 pode ser sobrescrito)
 	loadn r2, #49
 	cmp r1, r2
 	jeq _store
@@ -72,6 +77,7 @@ loop:
 fim:
 
 	jmp loop
+
 
 ; Instruções de manipualação de dado
 _store:
