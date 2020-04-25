fc04559a74b8dc4ba8a1474096cf60d0b549785d
Edws <eduardos.rocha17@gmail.com>
2020-04-25 09:19:05 -0300

    Store completado
    O simulador consegue executar a instrução store

--- simulador.asm
+++ simulador.asm
@@ -6,9 +6,9 @@
 
 ; Inicializa Stack com o valor que está em sp
 mov r0, sp ; Pega o valor da stack, pois nela iniciará a stack do programa a ser simulado
-sub r0, #code ; Transforma o valor real do Stack Pointer para o valor virtual
+loadn r1, #code
+sub r0, r0, r1 ; Transforma o valor real do Stack Pointer para o valor virtual
 store Stack, r0
-
 jmp loop
 
 Stack: var #1 ; Guarda o Stack Pointer do programa simulado
@@ -33,7 +33,7 @@ loop:
 	; Pega o valor que Program Counter aponta
 	load r1, PrgC ; Valor real do Program Counter
 	loadn r2, #code
-	add r2, r1, r2 ; Transforma o valor virtual do Program counter para o valor real
+	add r1, r1, r2 ; Transforma o valor virtual do Program counter para o valor real
 	loadi r0, r1 ; Carrega valor apontado pelo Program Counter (valor real)
 
 	; Incrementa valor virtual do Program Counter e o salva na memória
@@ -84,15 +84,31 @@ _store:
 	mov r1, r0
 	rotl r1, #6
 	shiftr0 r1, #13
-	add r1, #Regs
+	loadn r2, #Regs
+	add r1, r1, r2 ; r1 contém onde está salvo RX
 
-	; r1 contem onde está salvo RX
+	loadi r1, r1
 
-	loadi r2, r1
+	; r1 contém o conteúdo de RX
 
-	; r2 contem o conteúdo de RX
+	; Pega o valor que Program Counter aponta
+	load r3, PrgC ; Valor real do Program Counter
+	loadn r4, #code
+	add r3, r3, r4 ; Transforma o valor virtual do Program counter para o valor real
+	loadi r2, r3 ; Carrega valor apontado pelo Program Counter (valor real)
 
+	; Incrementa valor virtual do Program Counter e o salva na memória
+	inc r4
+	store PrgC, r4
 
+	; r2 contém o endereço real
+
+	loadn r3, #code
+	add r2, r2, r3
+
+	; r2 contém o endereço real
+
+	storei r2, r1
 
 	jmp fim
 
